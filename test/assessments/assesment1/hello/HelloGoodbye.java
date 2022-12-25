58508b67c0f379919e4de15cae133e91391bd8d1
Hardik Hajela <hardik.hajela@icloud.com>
2022-12-25 15:45:14 -0500

    Q2 of A1

--- test/assessments/assesment1/hello/HelloGoodbye.java
+++ test/assessments/assesment1/hello/HelloGoodbye.java
@@ -1,5 +1,13 @@
-package test.assessments.assesment1.hello;
+//package test.assessments.assesment1.hello;
+
+import java.util.Scanner;
 
 public class HelloGoodbye {
-    
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        String a= sc.next(), b= sc.next();
+
+        System.out.println("Hello "+a+" and "+b+".");
+        System.out.println("Goodbye "+b+" and "+a+".");
+    }
 }
